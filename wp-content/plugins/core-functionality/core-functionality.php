6df11871ace7503f0849fcdeb95e165b8583ab2f
Noah <noahkramer@Noahs-MBP.fios-router.home>
2017-12-02 15:05:37 -0500

    test

--- wp-content/plugins/core-functionality/core-functionality.php
+++ wp-content/plugins/core-functionality/core-functionality.php
@@ -3,12 +3,7 @@
  * Plugin Name: Core Functionality
  * Description: This contains all your site's core functionality so that it is theme independent. It is based on Bill Erickson's core functionality plugin: https://github.com/billerickson/Core-Functionality
  *
- * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
- * General Public License version 2, as published by the Free Software Foundation.  You may NOT assume 
- * that you can use any other version of the GPL.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
  *
  */
 
