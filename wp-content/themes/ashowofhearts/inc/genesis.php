71ad0cd7d39a106fcb92524e81b7337bb95bb6ee
Noah <noahkramer@Noahs-MBP-2.fios-router.home>
2018-02-08 11:23:20 -0500

    strip p tags from img

--- wp-content/themes/ashowofhearts/inc/genesis.php
+++ wp-content/themes/ashowofhearts/inc/genesis.php
@@ -550,3 +550,11 @@ function sp_genesis_title_comments() {
 		return $plugins;
 	}
 	add_filter( 'option_active_plugins', 'ea_disable_acf_on_frontend' );
+
+// Remove P tags from IMG
+
+function filter_ptags_on_images($content){
+	return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
+}
+
+add_filter('the_content', 'filter_ptags_on_images');
